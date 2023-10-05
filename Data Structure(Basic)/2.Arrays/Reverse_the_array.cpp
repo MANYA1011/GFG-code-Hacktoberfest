d93520fe2645e51da6697600af416d23549dfb71
manya <93120998+MANYA1011@users.noreply.github.com>
2023-10-05 19:26:33 +0530

    Update Reverse_the_array.cpp

--- Data Structure(Basic)/2.Arrays/Reverse_the_array.cpp
+++ Data Structure(Basic)/2.Arrays/Reverse_the_array.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
-void reverse(int arr[], int n){
-    int low=0,high=n-1;
+void reverse(int arr[], int len){
+    int low=0,high=len-1;
     while(low<high){
         int temp=arr[low];
         arr[low]=arr[high];
@@ -9,7 +9,7 @@ void reverse(int arr[], int n){
         low++;
         high--;
     }
-    for(int i=0; i<n; i++)
+    for(int i=0; i<len; i++)
     cout<<arr[i]<<" ";
 }
 int main(){
